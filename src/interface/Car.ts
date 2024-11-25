0c1e4ac2b995b3a9d287e37583b1fc9f2757d58d
Mateusz Cwojdziński <mateuszcwojdzinski@outlook.com>
2024-11-25 23:49:35 +0100

    Add new field and a NewCar interface for TS error prevention

--- src/interface/Car.ts
+++ src/interface/Car.ts
@@ -1,6 +1,15 @@
 export interface Car {
-    _id: string,
-    brand: string,
-    model: string,
-    year: string
-} 
+    _id: string;
+    brand: string;
+    model: string;
+    year: number;
+    transmission: "Manualna" | "Automatyczna";
+  }
+  
+  export interface NewCar {
+    brand: string;
+    model: string;
+    year: number;
+    transmission: "Manualna" | "Automatyczna";
+  }
+  
